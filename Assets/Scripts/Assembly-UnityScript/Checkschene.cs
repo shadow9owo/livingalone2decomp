3abab430832e404ca9261af938cb9973362623ed
shadowdev <69159493+shadow9owo@users.noreply.github.com>
2025-06-18 00:00:48 +0200

    Add files via upload

---
+++ Assets/Scripts/Assembly-UnityScript/Checkschene.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class Checkschene : MonoBehaviour
+{
+	public virtual void Start()
+	{
+	}
+
+	public virtual void Update()
+	{
+	}
+
+	public virtual void Main()
+	{
+	}
+}
